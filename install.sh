7e39c4a8e5610c695a29f33a513785eb9936b4d3
ryomtmt <ryomtmt@macbookair.local>
2015-01-19 17:54:41 +0900

    add dotfile simlink block add vim plugin update block

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd $(dirname $0)/src
+for dotfile in .?*
+do
+  if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+  then
+    ln -fs "$PWD/$dotfile" $HOME
+  fi
+done
+
+cd $(dirname $0)/src
 for dotfile in ?*
 do
   if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
@@ -9,6 +18,14 @@ do
   fi
 done
 
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-vim +":NeoBundleInstall" +:q
+if [ -d ~/.vim/bundle ]
+then
+  cd ~/.vim/bundle/neobundle.vim
+  git fetch
+  vim +":NeoBundleUpdate!" +:q
+else
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+  vim +":NeoBundleInstall" +:q
+fi
+
